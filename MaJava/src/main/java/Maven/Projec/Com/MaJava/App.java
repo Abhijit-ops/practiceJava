921229d6176a19891bf418031904bc615fb8a960
myname <abhijitkalita1984@gmail.com>
2020-10-13 21:43:48 +0530

    First Practice commit

---
+++ MaJava/src/main/java/Maven/Projec/Com/MaJava/App.java
@@ -0,0 +1,13 @@
+package Maven.Projec.Com.MaJava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ MaJava/src/test/java/Maven/Projec/Com/MaJava/casetest.java
@@ -0,0 +1,7 @@
+package Maven.Projec.Com.MaJava;
+
+import junit.framework.TestCase;
+
+public class casetest extends TestCase {
+
+}
